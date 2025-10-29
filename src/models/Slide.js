764953875b501e304a24294007a6a0a4eaf27078
𝓝𝓪𝓱𝓲𝓭 ☠️ <alimnh412@gmail.com>
2025-10-29 11:40:08 +0600

    add

--- src/models/Slide.js
+++ src/models/Slide.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const slideSchema = new mongoose.Schema({
-    text: { type: String, required: true },
     imageUrl: { type: String, required: true },
     public_id: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
